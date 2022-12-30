9fb0fbd45eec9a96fef54fd8be714e073a44db14
Andrew Ramires May <andrewc3po@gmail.com>
2022-12-30 09:45:13 -0400

    Adição de BPL_IDAssignedToInvoice

--- src/app/model/document-payment.ts
+++ src/app/model/document-payment.ts
@@ -2,6 +2,7 @@ import { Document } from './document';
 import { DocumentLine } from './document-line';
 
 export class DocumentPayment extends Document {
+  BPL_IDAssignedToInvoice = 2
   constructor(cardCode: String, documentLines: Array<DocumentLine>) {
     super();
     this.CardCode = cardCode;
@@ -10,6 +11,7 @@ export class DocumentPayment extends Document {
       .map((it) => it.LineTotal)
       .reduce((sum, current) => sum + current, 0);
     this.BPLId = 2;
+    this.BPL_IDAssignedToInvoice = 2
   }
   DownPaymentType: String = 'dptInvoice';
 }
