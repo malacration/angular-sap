8993e14d8c841f8739c4827c7f93ef20f59704d6
Andrew <andrewc3po@gmail.com>
2023-01-04 17:29:46 -0400

    correção

--- src/app/model/importacao/constantes.ts
+++ src/app/model/importacao/constantes.ts
@@ -5,4 +5,4 @@
 //colocar data de vencimento do perioro de 12/2022 para ate final de 2031
 //USO0000402 - USO0000528
 export const COD_IMPOSTO = "zero"
-export const COD_ITEM = "USO0000402"
+export const COD_ITEM = "USO0000528"
