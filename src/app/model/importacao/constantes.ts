fbe44c3c5801955c3a1dd9f4627903b648c43034
Andrew <andrewc3po@gmail.com>
2023-01-04 16:01:14 -0400

    correção

--- src/app/model/importacao/constantes.ts
+++ src/app/model/importacao/constantes.ts
@@ -3,6 +3,6 @@
 //configurar os depositos
 //validar a conta contabil
 //colocar data de vencimento do perioro de 12/2022 para ate final de 2031
-
+//USO0000402 - USO0000528
 export const COD_IMPOSTO = "zero"
-export const COD_ITEM = "USO0000528"
+export const COD_ITEM = "USO0000402"
