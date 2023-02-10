d7976d3a54910cf82c5d05a9a9474915d85af949
Andrew <andrewc3po@gmail.com>
2023-02-10 15:03:43 -0400

    correção nome

--- src/app/service/config-service.ts
+++ src/app/service/config-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-  private host = 'https://saphaslrovema:50000';
+  private host = 'https://sapharovema:50000';
 
   getHost(): String {
     return this.host;
