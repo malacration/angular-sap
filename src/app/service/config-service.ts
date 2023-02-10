1202266a6cde22771d18423a2432fe0d37689b38
Andrew <andrewc3po@gmail.com>
2023-02-10 14:23:41 -0400

    correção

--- src/app/service/config-service.ts
+++ src/app/service/config-service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ConfigService {
-  private host = 'https://hanab1:50000';
+  private host = 'https://saphaslrovema:50000';
 
   getHost(): String {
     return this.host;
